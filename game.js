0d61d1111b2c32c346f112b8b3c83a248c368e12
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-07-18 22:49:19 -0400

    added comments

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// Randomly selects a word for the player
+
 exports.randomizer = {
 	wordChoices: ['ORIENTAL AVENUE','BOARDWALK','PARK PLACE', 'READING RAILROAD', 'ILLINOIS AVENUE', 'NEW YORK AVENUE', 'VERMONT AVENUE', 'CONNECTICUT AVENUE', 'PENNSYLVANIA AVENUE', 'SHORT LINE', 'ELECTRIC COMPANY'],
 	WordChooser: function(randomNum) {
